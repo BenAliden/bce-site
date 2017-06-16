33311e85dd7d8d17558a889bdbeb04e002ea6ed9
BenAliden <danielbboman@gmail.com>
2017-06-16 13:48:55 -0400

    Add files via upload

---
+++ pricing.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/normalize.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<!-- Header -->
+<div id="all-wrapper">
+	<div class="main-wrapper">
+		<div class="logo-wrapper">
+			<a href="./"><img class="bme-logo" src="../BrandIdentity/WebsiteHeader_v00.png"></a>
+		</div>
+		<div class="menu-bar">
+			<div id="menu-btn">
+				<img src="images/hamburger.png">
+			
+				<div id="menu">
+					<ul>
+						<a href="results.php"><li>Results</li></a>
+						<a href="howitworks.php"><li>How it Works</li></a>
+						<a href="pricing.php"><li>Pricing</li></a>
+						<a href="newevent.php"><li>Host an Event</li></a>
+					</ul>
+				</div>
+			</div>
+			<div style="float:right;">
+				<ul>
+					<a href="login.php"><li>Log In</li></a>
+					<a href="signup.php"><li>Sign Up</li></a>
+				</ul>
+			</div>
+		</div>
+		
+		<!-- Main Body -->
+		<div class="main-content">
+			<h1>Pricing</h1>
+			<p>Below you can find prices for the services we offer. To plan out what you need for your event, see the <a href="newevent.php">event hosting page</a>. </p>
+			
+			<div class="pricing-wrapper">
+				<div>
+					<div>$50</div>
+				</div>
+				<div>
+					<h3>Event Hosting</h3>
+					<div>Database and interfaces for registration, check in, MC, deck captains, and judging. After the event, results are stored on our site for future access.</div>
+				</div>
+			</div>
+			<div class="pricing-wrapper">
+				<div>
+					<div>$5</div>
+					<div class="pricing-mod">per day plus shipping</div>
+				</div>
+				<div>
+					<h3>Tablet Rental</h3>
+					<div>7" Android tablets for use by event officials</div>
+				</div>
+			</div>
+			<div class="pricing-wrapper">
+				<div>
+					<div>$10</div>
+					<div class="pricing-mod">per day plus shipping</div>
+				</div>
+				<div>
+					<h3>Hotspot Rental</h3>
+					<div>WiFi hotspot to allow tablets to connect to the system. Each hotspot runs on either Verizon or AT&amp;T and supports up to 10 devices.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Footer -->
+<div id="footer">
+	<div id="copyright">&copy; 2017 Ballroom Comp Express</div>
+</div>
+</body>
+</html>
